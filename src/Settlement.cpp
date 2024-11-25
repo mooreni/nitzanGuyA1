b4f59e5e0488e984af88e08d2575488f4bd98aa6
Guy <guy12a@gmail.com>
2024-11-25 10:47:03 +0000

    SettlementSetup

---
+++ src/Settlement.cpp
@@ -0,0 +1,20 @@
+#include "Settlement.h"
+
+Settlement::Settlement(const string &name, SettlementType type)
+{
+}
+
+const string &Settlement::getName() const
+{
+    // TODO: insert return statement here
+}
+
+SettlementType Settlement::getType() const
+{
+    return SettlementType();
+}
+
+const string Settlement::toString() const
+{
+    return string();
+}
